5c8a62f1566d8f59bab50cee9d78f8e75445e376
Ayush Kathariya <ayushkathariya108@gmail.com>
2024-01-07 15:31:05 +0545

    initial commit

---
+++ src/api/order/routes/custom-router.js
@@ -0,0 +1,12 @@
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/orders/customOrder",
+      handler: "order.customOrderController",
+      config: {
+        policies: [],
+      },
+    },
+  ],
+};
